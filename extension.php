ff031d14c3473a41c8cd1fa6cb912a44e06c94ef
cdowdy <cdowdy46@gmail.com>
2014-08-28 10:07:43 -0400

    first commit

---
+++ extension.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * WebFont Loader for Bolt CMS
+ * uses Typekit's WebFont loader to load Typekit, Google Fonts or locally hosted fonts
+ *
+ */
+namespace WebfontLoader;
+
+class Extension extends \Bolt\BaseExtension
+{
+
+}
